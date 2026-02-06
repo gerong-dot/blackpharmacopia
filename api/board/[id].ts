604c1efd864a368a9799ed253ad946deab61ebbe
Squarecat-meow <yozumina@serafuku.moe>
2026-02-07 00:49:00 +0900

    feat(Post): 메타데이터와 페이지 fetch & parse

--- api/board/[id].ts
+++ api/board/[id].ts
@@ -1,11 +1,17 @@
-import { notion } from '../_lib/notion.ts';
+import { ExtendedRecordMap } from 'notion-types';
+import { notion, notionHQ } from '../_lib/notion.ts';
+import type { IPostMetadata } from '../../dto/notion';
 
 export default async function handler(req, res) {
   const { id } = req.query;
   try {
-    const results = await notion.getPage(id);
+    const [metadata, recordMap]: [IPostMetadata, ExtendedRecordMap] =
+      await Promise.all([
+        notionHQ.pages.retrieve({ page_id: id }) as Promise<IPostMetadata>,
+        notion.getPage(id),
+      ]);
 
-    res.status(200).json(results);
+    res.status(200).json({ metadata, recordMap });
   } catch (e) {
     res.status(500).json({ error: `내부 서버 에러: ${e}` });
   }
--- src/pages/Board/Post.tsx
+++ src/pages/Board/Post.tsx
@@ -4,23 +4,46 @@ import { Loader2Icon } from 'lucide-react';
 import type { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
 import { useParams } from 'react-router';
+import type { IPostMetadata } from '../../../dto/notion';
 import Authors from '../../components/Authors';
 
 function Post() {
   const { id } = useParams();
-  const {
-    data: recordMap,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['post', id],
-    queryFn: () => ky.get<ExtendedRecordMap>(`/api/board/${id}`).json(),
+    queryFn: () =>
+      ky
+        .get<{
+          metadata: IPostMetadata;
+          recordMap: ExtendedRecordMap;
+        }>(`/api/board/${id}`)
+        .json(),
   });
 
+  if (!data || isError) {
+    return <h1>{error?.message}</h1>;
+  }
+
+  const { properties } = data.metadata;
+
   return (
+    <>
+      {!isLoading && data ? (
+        <div className="flex flex-col items-center p-4 bg-white grow rounded-xl">
+          <h1 className="font-noto-serif text-4xl font-bold">
+            {properties.제목.title[0].plain_text}
+          </h1>
           <div className="flex items-center gap-2 mb-2">
             <Authors authors={properties.작성자.people} />
+            <span className="text-slate-400">|</span>
+            <span className="text-xs text-slate-500">
+              {new Date(properties.작성일.created_time).toLocaleString(
+                'ko-KR',
+                {
+                  timeZone: 'Asia/Seoul',
+                },
+              )}
+            </span>
           </div>
           <NotionRenderer
             recordMap={data?.recordMap}
