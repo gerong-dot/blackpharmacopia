8de3561935c210e773230269fd3c57853b640445
Squarecat-meow <yozumina@serafuku.moe>
2026-02-07 00:43:31 +0900

    @notionhq/client 설정

--- api/_lib/notion.ts
+++ api/_lib/notion.ts
@@ -1,3 +1,7 @@
+import { Client } from '@notionhq/client';
 import { NotionAPI } from 'notion-client';
 
+export const notionHQ = new Client({
+  auth: process.env.VITE_NOTION_SECRET_KEY,
+});
 export const notion = new NotionAPI();
