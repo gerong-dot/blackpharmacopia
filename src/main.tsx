9d75d0cc876973deaba82372d8af460688d342eb
Squarecat-meow <yozumina@serafuku.moe>
2026-02-07 01:09:24 +0900

    fix(App): queryClient의 staleTime 변경

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
